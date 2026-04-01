de0732120dc290e9021f71821d3d6d4936f1960e
BaderAlharbiQUT <n11866951@qut.edu.au>
2026-04-01 09:10:53 +0000

    connect student routes to server

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,13 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
-
+const studentRoutes = require('./routes/studentRoutes');
 dotenv.config();
 
 
 const app = express();
 
+app.use('/api/students', studentRoutes);
 app.use(cors());
 app.use(express.json());
 app.use('/api/auth', require('./routes/authRoutes'));
